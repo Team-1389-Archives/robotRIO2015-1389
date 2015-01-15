b14b115f8ee7203bc827ec9af7cb6e6c5ee8b39f
Paul LoBuglio <paullo101123@gmail.com>
2015-01-15 17:53:31 -0500

    Readjust tote auton

--- MyRobot.cpp
+++ MyRobot.cpp
@@ -1,25 +1,219 @@
 #include "WPILib.h"
 #include "controller-macros.h"
+#include <ctime>
+#include <math.h>
 
-class MyRobot: public SampleRobot
-{
+
+
+//Port Numbers
+#define ENCODER_ONE_DIGIPORTS 6, 7
+#define ENCODER_TWO_DIGIPORTS 8, 9
+#define LIMIT_ONE_DIGIPORT 0
+#define	LIMIT_TWO_DIGIPORT 1
+#define FR_PWM_DRIVE_PORT 0
+#define FL_PWM_DRIVE_PORT 1
+#define BR_PWM_DRIVE_PORT 2
+#define BL_PWM_DRIVE_PORT 3
+#define ELEVATOR_PWM_PORT 4
+#define ELEVATOR_TWO_PWM_PORT 5
+#define DRIVE_JOYSTICK_PORT 0
+#define FUNC_JOYSTICK_PORT 1
+//#define TEST_VICTOR_PORT 4
+//Constants used
+#define ELEVATOR_SPEED 1
+#define GLOBAL_SPEED_MODIFIER 1
+#define FOUR_WHEEL_LIMITER 1.41421356237
+
+class MyRobot: public SampleRobot {
+
+	int iterationNumber;
+	float currTime = 0;
+	Timer* threadTime;
+	bool fin = false;
+	Joystick *driver, *func;
+	DigitalInput *IRa, *IRb, *IRc, *IRd, *IRe, *limitOne, *limitTwo;
+	Talon *FRdrive, *FLdrive, *BRdrive, *BLdrive;
+	Victor *testMotor;
+	Victor *elevatorOne, *elevatorTwo;
+	RobotDrive *drive;
+	Encoder *cody, *codec;
 
 public:
 	MyRobot()
+
 {
-
+		mainInstance();
+		//ConfigureEncoders();
 }
+	void Autonomous() {
+		const double defaultSpeed = -0.5;
+		while(!limitOne->Get() && ! limitTwo->Get() && IsAutonomous()){
+			drive->ArcadeDrive(defaultSpeed, 0);
+		}
 
-	void Autonomous()
+		while(IsAutonomous()){
+			double turn = 0.0;
+			double speed = defaultSpeed;
+			if(!limitOne->Get()){
+				turn -= 0.25;
+				speed = 0.2;
+			}
+			if(!limitTwo->Get()){
+				turn += 0.25;
+				speed = 0.2;
+			}
+			drive->ArcadeDrive(speed, turn);
+		}
+	}
+
+	void OperatorControl() {
+		threadTime->Start(); //mr chen was here
+		while (IsOperatorControl()) {
+
+			SmartDashboard::PutNumber("PWMValue", codec->GetRate());
+			//SmartDashboard::PutNumber("PWM Value", tester->GetRaw());
+			SmartDashboard::PutNumber("Limit One value", limitOne->Get());
+			SmartDashboard::PutNumber("Limit Two value", limitTwo->Get());
+			//	SmartDashboard::PutNumber("Encoder One value", cody->GetRate());
+			//	SmartDashboard::PutNumber("Encoder Two value", codec->GetRate());
+			SmartDashboard::PutNumber("LeftAnalog X axis",
+					driver->GetRawAxis(LeftX));
+			SmartDashboard::PutNumber("LeftAnalog Y axis",
+					driver->GetRawAxis(LeftY));
+
+			elevatorOne->Set(rampUp(1,12));
+
+			//LiftControl();
+			FourCIMDrive();
+		}
+
+		threadTime->Reset();
+	}
+
+
+
+
+
+
+
+
+
+	void LiftControl() {
+		float rightAnalogPush = func->GetRawAxis(RightY) * -1;
+		int direction = 0;
+
+		if (rightAnalogPush > .5
+				&& !IRa->Get()) {
+			direction = 1;
+			elevatorOne->Set(ELEVATOR_SPEED);
+		}
+		if (func->GetRawButton(BumperR)
+				&& !IRb->Get()) {
+			direction = 1;
+			elevatorOne->Set(ELEVATOR_SPEED);
+		}
+		if (func->GetRawButton(BumperL)
+				&& !IRc->Get()) {
+			direction = 1;
+			elevatorOne->Set(ELEVATOR_SPEED);
+		}
+		if (func->GetRawAxis(3) > .4
+				&& !IRd->Get()) {
+			direction = 1;
+			elevatorOne->Set(ELEVATOR_SPEED);
+		}
+		if (func->GetRawAxis(3) < -.4
+				&& !IRe->Get()) {
+			direction = 1;
+			elevatorOne->Set(ELEVATOR_SPEED);
+		}
+		if (IRa->Get() || IRb->Get() || IRc->Get() || IRd->Get() || IRe->Get())
+		{
+			elevatorOne->Set(direction);
+			Wait(0.2);
+		}
+		if (rightAnalogPush < -.5 && !IRa->Get())
+		{
+			direction = -1;
+			elevatorOne->Set(ELEVATOR_SPEED * -1);
+		}
+	}
+
+	void ConfigureEncoders() {
+		cody->SetMaxPeriod(.1);
+		cody->SetMinRate(10);
+		cody->SetDistancePerPulse(5);
+		cody->SetReverseDirection(true);
+		cody->SetSamplesToAverage(7);
+
+		codec->SetMaxPeriod(.1);
+		codec->SetMinRate(10);
+		codec->SetDistancePerPulse(5);
+		codec->SetReverseDirection(true);
+		codec->SetSamplesToAverage(7);
+	}
+
+	int abs(float num)
 	{
+		if (num < 0)
+			return num * -1;
+		return num;
+	}
+
+	float rampUp(float maxPower, float time)
+	{
+
+		currTime = threadTime->Get();
+		float power = 0;
+		SmartDashboard::PutNumber("TIME", currTime);
+		bool fin = currTime >= time;
+		if (!fin)
+		{
+			power = (-2 * maxPower / pow(time, 3) * pow(currTime, 3)) + (3 * maxPower / pow(time,2) * pow(currTime, 2));
+			SmartDashboard::PutNumber("MotorPower", power);
+			return power;
+		}
+		else if (fin)
+		{
+			SmartDashboard::PutNumber("fin power: " , power);
+			return maxPower;
+		}
+		return 0;
+	}
+
+	void FourCIMDrive() {
+		float x, y;
+		x = driver->GetRawAxis(LeftX) * -1;
+		y = driver->GetRawAxis(LeftY);
+
+		//FRdrive ->Set((y - x) / FOUR_WHEEL_LIMITER);
+		//FLdrive ->Set((y + x) / FOUR_WHEEL_LIMITER);
+		//BRdrive ->Set((y - x) / FOUR_WHEEL_LIMITER);
+		//BLdrive ->Set((y + x) / FOUR_WHEEL_LIMITER);
+		drive->ArcadeDrive(y * GLOBAL_SPEED_MODIFIER,
+				x * GLOBAL_SPEED_MODIFIER);
 
 	}
 
-	void OperatorControl()
+	void mainInstance()
 	{
-
+		threadTime = new Timer();
+		threadTime->Reset();
+		cody = new Encoder(ENCODER_ONE_DIGIPORTS, false, Encoder::k4X);
+		codec = new Encoder(ENCODER_TWO_DIGIPORTS, false, Encoder::k4X);
+		limitOne = new DigitalInput(LIMIT_ONE_DIGIPORT);
+		limitTwo = new DigitalInput(LIMIT_TWO_DIGIPORT);
+		FRdrive = new Talon(FR_PWM_DRIVE_PORT);
+		FLdrive = new Talon(FL_PWM_DRIVE_PORT);
+		BRdrive = new Talon(BR_PWM_DRIVE_PORT);
+		BLdrive = new Talon(BL_PWM_DRIVE_PORT);
+		elevatorOne = new Victor(ELEVATOR_PWM_PORT);
+		elevatorTwo = new Victor(ELEVATOR_TWO_PWM_PORT);
+		func = new Joystick(FUNC_JOYSTICK_PORT);
+		driver = new Joystick(DRIVE_JOYSTICK_PORT);
+		testMotor = new Victor(6);
+		drive = new RobotDrive(FLdrive, BLdrive, FRdrive, BRdrive);
 	}
-
 
 };
 
--- controller-macros.h
+++ controller-macros.h
@@ -2,19 +2,19 @@
 #define CONTROLLER_MACROS_H
 
 // XBOX Controller button and axis numbers
-#define ControllerA			(1) // Port for primary controller, drive controller
-#define ControllerB			(2) // Port for secondary controller, functionality
+#define ControllerA			(0) // Port for primary controller, drive controller
+#define ControllerB			(1) // Port for secondary controller, functionality
 
-#define ButtonX 			(3) // XBox Controller X Button number for getRawButton() or getRawAxis()
-#define ButtonA				(1) // XBox Controller A Button number
-#define ButtonB				(2) // XBox Controller B Button number
-#define ButtonY				(4) // XBox Controller Y Button number
-#define BumperL				(5) // XBox Controller  Left Bumper number
-#define BumperR				(6) // XBox Controller Right Bumper number
+#define ButtonX 			(2) // XBox Controller X Button number for getRawButton() or getRawAxis()
+#define ButtonA				(0) // XBox Controller A Button number
+#define ButtonB				(1) // XBox Controller B Button number
+#define ButtonY				(3) // XBox Controller Y Button number
+#define BumperL				(4) // XBox Controller  Left Bumper number
+#define BumperR				(5) // XBox Controller Right Bumper number
 
-#define LeftY				(2) // XBox Controller  Left Y Axis number
-#define LeftX				(1) // XBox Controller  Left X Axis number
-#define RightY				(5) // XBox Controller Right Y Axis number
-#define RightX				(4) // XBox Controller Right X Axis number
+#define LeftY				(1) // XBox Controller  Left Y Axis number
+#define LeftX				(0) // XBox Controller  Left X Axis number
+#define RightY				(4) // XBox Controller Right Y Axis number
+#define RightX				(3) // XBox Controller Right X Axis number
 
 #endif
